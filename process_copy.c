0ab7cd5c5c9c27fdf177b80962bf8e03b7497c7a
ReaganDun <ds123314@163.com>
2021-07-16 18:17:04 +0800

    first version

---
+++ process_copy.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<sys/wait.h>
+#include<signal.h>
+
+int main(int argc,char ** argv)
+{
+	if(argc < 3)
+	{
+		printf("缺少必要命令行参数！\n");
+	}
+
+	int processnum = 10;
+	if(argv[3] != NULL)
+	{
+		processnum = atoi(argv[3]);
+		if(processnum > 100 && processnum < 0)
+		{
+			printf("输入的进程数无效！必须于0到100之间\n");
+			return 0;
+		}
+	}
+
+	int everynum;
+	int i;
+	pid_t pid;
+	struct stat st;
+	stat(argv[1],&st);
+	
+	everynum = st.st_size/processnum;
+	for(i = 1;i<processnum+1;i++)
+	{
+		pid = fork();
+		if(pid == 0)
+			break;
+	}
+
+	if(pid > 0)
+	{
+
+	}else if(pid == 0)
+	{
+		if(st.st_size%processnum != 0 && i == processnum)
+			everynum = everynum + st.st_size%processnum;
+		char strbuf[20];
+		sprintf(strbuf, "%s %s %d %d",argv[1],argv[2],i,everynum);
+		execl("./mycp","mycp",strbuf,NULL);
+	}else
+	{
+		perror("fork error");
+	}
+
+	return 0;
+}
